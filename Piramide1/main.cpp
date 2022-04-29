c0a4144af67fa4b0f20988da78d7f0fdbd37eebb
Henry Giovanny Gallegos Velgara <hgallegos@utec.edu.pe>
2022-04-29 08:08:28 -0500

    agregando shader

--- Piramide1/main.cpp
+++ Piramide1/main.cpp
@@ -20,6 +20,37 @@ int indices2[] = {
         0, 2, 3,
         0, 1, 3
 };
+GLuint p_id;
+
+static void CreateShaderProgram (char* vertexShaderFile, char* fragmentShaderFile, GLuint &p_id) {
+    char*	vertexShader   = readShader(vertexShaderFile);
+    char*	fragmentShader = readShader(fragmentShaderFile);
+
+    /* vertex shader */
+    GLuint v_id = glCreateShader(GL_VERTEX_SHADER);
+    if (v_id == 0)
+        Error("Could not create vertex shader object");
+
+    glShaderSource(v_id, 1, (const char**) &vertexShader, 0);
+    CompileShader(v_id);
+
+    /* fragment shader */
+    GLuint f_id = glCreateShader(GL_FRAGMENT_SHADER);
+    if (f_id == 0)
+        Error("Could not create fragment shader object");
+
+    glShaderSource(f_id, 1, (const char**) &fragmentShader, 0);
+    CompileShader(f_id);
+
+    /* program */
+    p_id = glCreateProgram();
+    if (p_id == 0)
+        Error("Could not create program object");
+    glAttachShader(p_id, v_id);
+    glAttachShader(p_id, f_id);
+    LinkProgram(p_id);
+
+}
 
 
 void Redisplay(void) {
@@ -35,6 +66,9 @@ int main(int argc, char* argv[]) {
     glutInitWindowSize(400, 300);
 
     int nWindow = glutCreateWindow("test");
+
+    CreateShaderProgram("vertexShader.vs", "fragmentShader.fs", p_id);
+
 
     //glutKeyboardFunc( SampleKeyboard );
     glutDisplayFunc( Redisplay );
---
+++ Piramide1/vertexShader.vs
@@ -0,0 +1,28 @@
+#version 330 core
+layout (location = 0) in vec3 aPos;
+//layout (location = 1) in vec3 aNormal;
+
+//uniform mat4 matrix_model;
+
+//out vec4 normal;
+//out vec4 posicion;
+//out vec3 colorFinal;
+
+void main()
+{
+    gl_Position = vec4(aPos, 1.0);
+
+   //posicion = matrix_model * vec4(aPos, 1.0);
+   //gl_Position = posicion;
+
+   //normal = matrix_model * vec4(aNormal, 1.0);
+   //normal = normalize(normal);
+
+   // iluminacion difusa en vertex shader
+   //vec3 posicionLuz = vec3(1., 1., -10.);
+   //vec3 colorLuz = vec3(1., 1., 1.);
+   //vec4 color = vec4(1.0f, 0.5f, 0.2f, 1.0f);
+   //float kd = 0.8;
+   //float diff = max(0.0, dot(normalize(posicionLuz - aPos), aNormal));
+   //colorFinal = color.rgb * colorLuz * kd * diff;
+}
